52ade484f6c487668660c36a4768c95ef5688108
mannk <kostia.mannevy@gmail.com>
2017-06-21 10:36:52 +0200

    Suppression label photo selectionnee

--- src/ihm/FenetreSaisieVipPhoto.java
+++ src/ihm/FenetreSaisieVipPhoto.java
@@ -38,59 +38,55 @@ public class FenetreSaisieVipPhoto extends javax.swing.JDialog {
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+        private void initComponents() {
 
-        btValid = new javax.swing.JButton();
-        lbPhoto = new javax.swing.JLabel();
-        lbVip = new javax.swing.JLabel();
-        btAjout = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        laTable = new javax.swing.JTable();
-        lbSelectionPhoto = new javax.swing.JLabel();
-        lbPhotoSelect = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        laTablePhoto = new javax.swing.JTable();
-        lbSelection = new javax.swing.JLabel();
-        lbVips = new javax.swing.JLabel();
+                btValid = new javax.swing.JButton();
+                lbPhoto = new javax.swing.JLabel();
+                lbVip = new javax.swing.JLabel();
+                btAjout = new javax.swing.JButton();
+                jScrollPane1 = new javax.swing.JScrollPane();
+                laTable = new javax.swing.JTable();
+                jScrollPane2 = new javax.swing.JScrollPane();
+                laTablePhoto = new javax.swing.JTable();
+                lbSelection = new javax.swing.JLabel();
+                lbVips = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("Ajouter VIP");
-        setResizable(false);
-        setSize(new java.awt.Dimension(0, 0));
+                setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+                setTitle("Ajouter VIP");
+                setResizable(false);
+                setSize(new java.awt.Dimension(0, 0));
 
-        btValid.setText("Valider");
-        btValid.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btValidActionPerformed(evt);
-            }
-        });
+                btValid.setText("Valider");
+                btValid.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                btValidActionPerformed(evt);
+                        }
+                });
 
-        lbPhoto.setText("Photo :");
+                lbPhoto.setText("Photo :");
 
-        lbVip.setText("VIP :");
+                lbVip.setText("VIP :");
 
-        btAjout.setText("Ajouter");
-        btAjout.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btAjoutActionPerformed(evt);
-            }
-        });
+                btAjout.setText("Ajouter");
+                btAjout.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                btAjoutActionPerformed(evt);
+                        }
+                });
 
-        laTable.setModel(leModeleTable);
-        jScrollPane1.setViewportView(laTable);
+                laTable.setModel(leModeleTable);
+                jScrollPane1.setViewportView(laTable);
 
-        lbSelectionPhoto.setText("Photo selectionné:");
+                laTablePhoto.setModel(leModeleTPhoto);
+                jScrollPane2.setViewportView(laTablePhoto);
 
-        laTablePhoto.setModel(leModeleTPhoto);
-        jScrollPane2.setViewportView(laTablePhoto);
+                lbSelection.setText("VIP selectionné :");
 
-        lbSelection.setText("VIP selectionné :");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+                getContentPane().setLayout(layout);
+                layout.setHorizontalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addGroup(layout.createSequentialGroup()
@@ -111,19 +107,14 @@ public class FenetreSaisieVipPhoto extends javax.swing.JDialog {
                                                         .addGroup(layout.createSequentialGroup()
                                                                 .addGap(86, 86, 86)
                                                                 .addComponent(lbVips, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                                                        .addComponent(lbSelectionPhoto, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                                        .addComponent(lbPhotoSelect, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                                                .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGap(255, 255, 255)
                                                 .addComponent(btValid)))
                                 .addContainerGap(18, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                );
+                layout.setVerticalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(40, 40, 40)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -144,21 +135,17 @@ public class FenetreSaisieVipPhoto extends javax.swing.JDialog {
                                                                 .addComponent(btAjout)
                                                                 .addGap(150, 150, 150))
                                                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                                        .addComponent(lbSelectionPhoto, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                                        .addComponent(lbPhotoSelect, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                                         .addComponent(lbVip, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                                                                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
                                                                 .addGap(129, 129, 129))))))
-        );
+                );
 
-        getAccessibleContext().setAccessibleParent(this);
+                getAccessibleContext().setAccessibleParent(this);
 
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
+                pack();
+        }// </editor-fold>//GEN-END:initComponents
 
     private void btAjoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btAjoutActionPerformed
         // TODO add your handling code here:
@@ -204,18 +191,16 @@ public class FenetreSaisieVipPhoto extends javax.swing.JDialog {
     }
 
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btAjout;
-    private javax.swing.JButton btValid;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable laTable;
-    private javax.swing.JTable laTablePhoto;
-    private javax.swing.JLabel lbPhoto;
-    private javax.swing.JLabel lbPhotoSelect;
-    private javax.swing.JLabel lbSelection;
-    private javax.swing.JLabel lbSelectionPhoto;
-    private javax.swing.JLabel lbVip;
-    private javax.swing.JLabel lbVips;
-    // End of variables declaration//GEN-END:variables
+        // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JButton btAjout;
+        private javax.swing.JButton btValid;
+        private javax.swing.JScrollPane jScrollPane1;
+        private javax.swing.JScrollPane jScrollPane2;
+        private javax.swing.JTable laTable;
+        private javax.swing.JTable laTablePhoto;
+        private javax.swing.JLabel lbPhoto;
+        private javax.swing.JLabel lbSelection;
+        private javax.swing.JLabel lbVip;
+        private javax.swing.JLabel lbVips;
+        // End of variables declaration//GEN-END:variables
 }
